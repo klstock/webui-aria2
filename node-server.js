9a16bd2143ec4227b87c7a871c0435e6c8dd8baa
kong lin <wuyou7410@gmail.com>
2019-02-27 13:16:15 +0800

    Update node-server.js

--- node-server.js
+++ node-server.js
@@ -4,10 +4,44 @@ var http = require("http"),
   fs = require("fs");
 port = process.argv[2] || 8888;
 
+function downloadM3U8(request, response) {
+  var m3u8_url = "";
+  var notify_url = "";
+  var tmp_path = "";
+
+  // http.get 获取 m3u8_url  内容
+  // m3u8_url  内容 中的 所有 ts  写入 到  tmp_path 目录中 保持原文件名
+  // aria2.addUri 添加 所有 ts 下载任务
+  // 添加 定时器  定期检查 任务id 是否下载完成
+  // 任务完成 通知 notify_url 附带必要参数
+  response.writeHead(200, { "Content-Type": contentType });
+  response.write(JSON.stringify({ code: 0 }));
+  response.end();
+}
+
+function downloadMp4(request, response) {
+  var m3u8_url = "";
+  var notify_url = "";
+  var tmp_path = "";
+
+  // aria2.addUri 添加 mp4 下载任务
+  // 添加 定时器  定期检查 任务id 是否下载完成
+  // 任务完成 通知 notify_url 附带必要参数
+  response.writeHead(200, { "Content-Type": contentType });
+  response.write(JSON.stringify({ code: 0 }));
+  response.end();
+}
+
 http
   .createServer(function(request, response) {
     var uri = url.parse(request.url).pathname,
       filename = path.join(process.cwd(), "docs", uri);
+    if (uri == "/api/downloadM3U8") {
+      downloadM3U8(request, response);
+      return;
+    } else if (uri == "/api/downloadMp4") {
+      downloadMp4(request, response);
+    }
 
     var extname = path.extname(filename);
     var contentType = "text/html";
