1bf7382c949cda81d2cc4411b094874f1740860e
liuchaoyang <1329674620@qq.com>
2019-02-28 10:02:15 +0800

    暂存

--- node-server.js
+++ node-server.js
@@ -2,45 +2,88 @@ var http = require("http"),
   url = require("url"),
   path = require("path"),
   fs = require("fs");
+
+const Aria2 = require("aria2");
+const aria2 = new Aria2([
+  {
+    host: "localhost",
+    port: 6800,
+    secure: false,
+    secret: "",
+    path: "/jsonrpc"
+  }
+]);
 port = process.argv[2] || 8888;
 
-function downloadM3U8(request, response) {
-  var m3u8_url = "";
-  var notify_url = "";
-  var tmp_path = "";
-
-  // http.get 获取 m3u8_url  内容
-  // m3u8_url  内容 中的 所有 ts  写入 到  tmp_path 目录中 保持原文件名
-  // aria2.addUri 添加 所有 ts 下载任务
-  // 添加 定时器  定期检查 任务id 是否下载完成
-  // 任务完成 通知 notify_url 附带必要参数
-  response.writeHead(200, { "Content-Type": contentType });
-  response.write(JSON.stringify({ code: 0 }));
-  response.end();
+async function downloadM3U8(param, response) {
+  var m3u8_url = param.file_url ? param.file_url : "";
+  var notify_url = param.notify_url ? param.notify_url : "";
+  var tmp_path = param.tmp_path ? param.tmp_path : "";
+  var filename = param.file_name ? param.file_name : "";
+  var m3u8Pathname = tmp_path + "/" + filename;
+  var guid = "";
+  try {
+  } catch (err) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ code: 500, err: err }));
+    response.end();
+  }
 }
 
-function downloadMp4(request, response) {
-  var m3u8_url = "";
-  var notify_url = "";
-  var tmp_path = "";
+async function downloadMp4(param, response) {
+  var mp4_url = param.file_url ? param.file_url : "";
+  var notify_url = param.notify_url ? param.notify_url : "";
+  var tmp_path = param.tmp_path ? param.tmp_path : "";
+  var filename = param.file_name ? param.file_name : "";
+  var pathname = tmp_path + "/" + filename;
 
-  // aria2.addUri 添加 mp4 下载任务
+  var guid = "";
+  try {
+    guid = await aria2.call("addUri", [mp4_url], { dir: tmp_path });
+    var args = {
+      timer: null,
+      notify_url: notify_url,
+      guid: guid
+    };
+    if (guid) {
+      args.timer = setInterval(
+        (function(args) {
+          return async function() {
+            var obj = await aria2.call("tellStatus", args.guid);
+            //任务完成
+            if (obj.status == "complete") {
+              clearInterval(args.timer);
+            }
+          };
+        })(args),
+        5000
+      );
+    }
+  } catch (err) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ code: 500, err: err }));
+    response.end();
+  }
   // 添加 定时器  定期检查 任务id 是否下载完成
   // 任务完成 通知 notify_url 附带必要参数
-  response.writeHead(200, { "Content-Type": contentType });
-  response.write(JSON.stringify({ code: 0 }));
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.write(JSON.stringify({ code: 0, tast_id: guid }));
   response.end();
 }
 
 http
-  .createServer(function(request, response) {
-    var uri = url.parse(request.url).pathname,
-      filename = path.join(process.cwd(), "docs", uri);
-    if (uri == "/api/downloadM3U8") {
-      downloadM3U8(request, response);
+  .createServer(async function(request, response) {
+    var req = url.parse(request.url),
+      pathname = req.pathname,
+      filename = path.join(process.cwd(), "docs", pathname),
+      param = url.parse(decodeURI(request.url), true).query;
+
+    if (pathname == "/api/downloadM3U8") {
+      downloadM3U8(param, response);
       return;
-    } else if (uri == "/api/downloadMp4") {
-      downloadMp4(request, response);
+    } else if (pathname == "/api/downloadMp4") {
+      downloadMp4(param, response);
+      return;
     }
 
     var extname = path.extname(filename);
