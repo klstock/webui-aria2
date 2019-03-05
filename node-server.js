76c5303381291ce95bc976692f49dd0687a04115
liuchaoyang <1329674620@qq.com>
2019-03-05 11:35:26 +0800

    add

--- node-server.js
+++ node-server.js
@@ -36,8 +36,8 @@ async function downloadM3U8(param, response) {
           stream_id: stream_id
         });
       } else {
-        errors.forEach(i => {
-          downloadApi(urls, tmp_path, notify_url, stream_id);
+        msg.errors.forEach(i => {
+          downloadApi(i, tmp_path, notify_url, stream_id);
         });
       }
     });
