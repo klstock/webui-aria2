7265163515a80bbb53800af70af66bfea1db59e2
kong lin <wuyou7410@gmail.com>
2019-03-06 12:38:33 +0800

    fix

--- restart_aria2.sh
+++ restart_aria2.sh
@@ -2,7 +2,7 @@
 source /etc/profile
 ps -ef | grep 'aria2c' | grep -v grep | awk '{print "kill -9",$2}' | sh
 sleep 2
-/usr/local/webui-aria2
+cd /usr/local/webui-aria2
 nohup aria2c --enable-rpc --rpc-listen-all > ./logs/'aria2_log'`date +%y-%m-%d_%H%M%S`'.out' 2>&1 &
 
 #  01  4  *  *  *  sh /usr/local/webui-aria2/restart_aria2.sh > /tmp/restart_vss_aria2.log  2>&1
--- restart_dwn-server.sh
+++ restart_dwn-server.sh
@@ -2,7 +2,7 @@
 source /etc/profile
 ps -ef | grep 'dwn-server.js' | grep -v grep | awk '{print "kill -9",$2}' | sh
 sleep 2
-/usr/local/webui-aria2
+cd /usr/local/webui-aria2
 nohup node dwn-server.js > ./logs/'dwn_log'`date +%y-%m-%d_%H%M%S`'.out' 2>&1 &
 
 #  01  4  *  *  *  sh /usr/local/webui-aria2/restart_dwn.sh > /tmp/restart_vss_dwn.log  2>&1
