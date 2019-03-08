d2080a8849b879cfc7c04b08141a6802dd3926f3
kong lin <wuyou7410@gmail.com>
2019-03-08 12:25:30 +0800

    Update restart_dwn-server.sh

--- restart_dwn-server.sh
+++ restart_dwn-server.sh
@@ -5,5 +5,5 @@ sleep 2
 cd /usr/local/webui-aria2
 nohup node dwn-server.js > ./logs/'dwn_log'`date +%y-%m-%d_%H%M%S`'.out' 2>&1 &
 
-#  01  4  *  *  *  sh /usr/local/webui-aria2/restart_dwn.sh > /tmp/restart_vss_dwn.log  2>&1
-#  */2  *  *  *  * sh /usr/local/webui-aria2/check_dwn.sh > /tmp/check_vss_dwn.log  2>&1
+#  01  4  *  *  *  sh /usr/local/webui-aria2/restart_dwn-server.sh > /tmp/restart_vss_dwn.log  2>&1
+#  */2  *  *  *  * sh /usr/local/webui-aria2/check_dwn-server.sh > /tmp/check_vss_dwn.log  2>&1
